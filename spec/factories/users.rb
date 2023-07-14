28fb51017450c90ce44b120d3c0e5b4197e1de86
crufn <o12041@icloud.com>
2023-07-14 19:11:58 +0900

    テストコード完成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    family_name           { "田中" }
+    family_kata           { "タナカ" }
+    first_name            { "太郎" }
+    first_kata            { "タロウ" }
+    birthday              { Date.today }
   end
 end
