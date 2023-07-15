3eed4c3e93b06e4cfc449c634b9f10de40e99429
crufn <o12041@icloud.com>
2023-07-15 17:45:39 +0900

    レビュー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :purchase_records
+  # has_many :items
+  # has_many :purchase_records
 
   KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
+  validates :family_name, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/ }
   validates :family_kata, presence: true, format: { with: KATAKANA_REGEXP }
-  validates :first_name, presence: true
+  validates :first_name, presence: true, format: { with: /\A[\p{Han}\p{Hiragana}\p{Katakana}]+\z/ }
   validates :first_kata, presence: true, format: { with: KATAKANA_REGEXP }
-  validates :email, presence: true
   validates :birthday, presence: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
 
 end
