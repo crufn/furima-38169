071a983540840d41ad2026c01b736fad998f2bd1
crufn <o12041@icloud.com>
2023-07-15 18:07:30 +0900

    修正2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe User, type: :model do
   end
 
 describe 'ユーザー新規登録' do
-  context '異常なデータの場合' do
+  context '正常' do
+    it 'nicknameとemail、passwordとpassword_confirmation、family_nameとfamily_kata、first_nameとfirst_kata、birthdayが存在すれば登録できる' do
+      expect(@user).to be_valid
+    end
+  end
+
+  context '異常' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       expect(@user).not_to be_valid
@@ -49,7 +55,8 @@ describe 'ユーザー新規登録' do
       another_user = FactoryBot.build(:user)
       another_user.email = @user.email
       another_user.valid?
-      expect(another_user.errors.full_messages).to include('Email has already been taken')    end
+      expect(another_user.errors.full_messages).to include('Email has already been taken')    
+    end
     it 'emailは@を含まないと登録できない' do
       @user.email = 'invalid_email'
       expect(@user).not_to be_valid
@@ -58,14 +65,37 @@ describe 'ユーザー新規登録' do
       @user.password = '12345'
       @user.password_confirmation = '12345'
       expect(@user).not_to be_valid
-     end
-     it 'passwordは、半角英数字での入力が必須' do
+    end
+    it 'passwordは、半角英数字での入力が必須' do
       @user.password = 'パスワード'
       @user.password_confirmation = 'パスワード'
       expect(@user).not_to be_valid
-     end
-      it '名前はカタカナでの入力が必須であること' do        @user.family_kata = 'ひらがな'
+    end
+    it '名前はカタカナでの入力が必須であること' do       
+      @user.family_kata = 'ひらがな'
       @user.first_kata = 'ひらがな'
+      expect(@user).not_to be_valid
+    end
+    it '英字のみのパスワードでは設定できない' do
+      @user.password = 'password'
+      @user.password_confirmation = 'password'
+      expect(@user).not_to be_valid
+    end
+    it '数字のみのパスワードでは設定できない' do
+      @user.password = '123456'
+      @user.password_confirmation = '123456'
+      expect(@user).not_to be_valid
+    end
+    it '姓（全角）に半角文字が含まれていると登録できない' do
+      @user.family_name = 'Yamada'
+      expect(@user).not_to be_valid
+    end
+    it '名（全角）に半角文字が含まれていると登録できない' do
+      @user.first_name = 'Taro'
+      expect(@user).not_to be_valid
+    end
+    it '姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない' do
+      @user.family_kata = 'やまだ'
       expect(@user).not_to be_valid
     end
   end
