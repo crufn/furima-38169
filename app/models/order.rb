70b03920ff5e64a3d7725f20d32ad2ab6d9f8461
crufn <o12041@icloud.com>
2023-08-25 15:18:07 +0900

    モデルとコントローラーの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,19 +1,17 @@
-class Order < ApplicationRecord
 
   include ActiveModel::Model
   attr_accessor :region_id, :city, :address, :building_name, :building_name, :phone_number, :postal_code, :user_id, :item_id
 
   with_options presence: true do
-    validates :region_id, presence: true
-    validates :city, presence: true
-    validates :address, presence: true
-    validates :phone_number, presence: true
-    validates :postal_code, presence: true
+    validates :region_id
+    validates :city
+    validates :address
+    validates :phone_number
+    validates :postal_code
   end
 
   def save
-    purchase_record = PurchaseRecord.create(item_id: self.item_id, user_id: self.user_id)
-    shipping_address = ShippingAddress.create(region_id: self.region_id, city: self.city, address: self.address, phone_number: self.phone_number, postal_code: self.postal_code)
+    purchase_record = PurchaseRecord.create(item_id: item_id, user_id: user_id)
+    ShippingAddress.create(region_id: region_id, city: city, address: address, phone_number: phone_number, postal_code: postal_code)
   end
 
-end
